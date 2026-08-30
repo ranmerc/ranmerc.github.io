2ebf7c8d6c3cf1c623f00e3149f9be6bf32186ab
Kamran Ansari <ranmerc@outlook.com>
2026-08-30 20:08:57 +0530

    basic shell of the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Montserrat",
+      cssVariable: "--font-montserrat",
+      fallbacks: ["sans-serif"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Playfair Display",
+      cssVariable: "--font-playfair-display",
+      fallbacks: ["serif"],
+    },
+  ],
 });
